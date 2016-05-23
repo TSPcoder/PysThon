c05e88e1e1132c1388dfe9fcdc01e0ee4a2fe1da
NicolasDutronc <nicolas.dutronc@orange.fr>
2016-05-24 00:30:06 +0200

    correction d'un bug qui n'affichait que la contrainte de plus grands xmax et ymax

--- Presentation/GraphFrame.py
+++ Presentation/GraphFrame.py
@@ -8,6 +8,7 @@ class GraphFrame(tk.Frame):
 
         f = Figure(figsize = (5, 5), dpi = 100)
         a = f.add_subplot(111)
+        print(type(a))
         xmax = 0
         ymax = 0
         for c in parent.constraints:
@@ -21,37 +22,17 @@ class GraphFrame(tk.Frame):
         for c in parent.constraints:
             coefs = c.coeffsConstraint
             if coefs[0] != 0 and coefs[1] != 0:
-                a.plot([0, xmax], [ymax, 0])
+                x = [0, coefs[2]/coefs[1]]
+                y = [coefs[2]/coefs[0], 0]
+                xbis = [0, xmax*coefs[0]]
+                ybis = [ymax*coefs[1], 0]
+                a.plot(x, y)
+                #a.fill_between(x, y, where=xbis + ybis <= coefs[2], facecolor='red')
             elif coefs[0] == 0 and coefs[1] != 0:
                 a.plot([0, xmax],[coefs[2]/coefs[1], coefs[2]/coefs[1]])
             elif coefs[0] != 0 and coefs[1] == 0:
                 a.plot([coefs[2]/coefs[0],coefs[2]/coefs[0]], [0,ymax])
 
-        #a.plot([1, 2, 3, 4])
-
-
         canvas = FigureCanvasTkAgg (f, self)
         canvas.show()
         canvas.get_tk_widget().pack(expand=True)
-'''
-    def draw_figure(self, figure):
-        xmax = 0
-        ymax = 0
-        for c in self.constraints:
-            print(type(c))
-            table = c.intersection()
-            if table[1]>xmax:
-                xmax = table[1]
-            if table[2]>ymax:
-                ymax = table[2]
-        plt.axis([0,xmax, 0, ymax])
-        for c in self.constraints:
-            coefs = c.coeffsConstraint
-            if coefs[0] != 0 and coefs[1] != 0:
-                plt.plot([0, xmax], [ymax, 0])
-            elif coefs[0] == 0 and coefs[1] != 0:
-                plt.plot([0, xmax],[coefs[2]/coefs[1], coefs[2]/coefs[1]])
-            elif coefs[0] != 0 and coefs[1] == 0:
-                plt.plot([coefs[2]/coefs[0],coefs[2]/coefs[0]], [0,ymax])
-        plt.show()
-        '''
